f5febf2adc53d115c75af7b03d0baa94a531d263
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2021-05-17 21:12:34 +0200

    Added cryptic favicon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from "./core/api.service";
-import { Namespace, ServiceQuery } from "./core/data";
+import {Component, OnInit} from '@angular/core';
+import {ApiService} from "./core/api.service";
+import {Namespace, ServiceQuery} from "./core/data";
 
 @Component({
   selector: 'app-root',
@@ -24,6 +24,15 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     const cryptic = window.location.hostname.includes('cryptic');
+
+    let link = document.querySelector<HTMLLinkElement>("link[rel~='icon']");
+    if (!link) {
+      link = document.createElement('link');
+      link.rel = 'icon';
+      link.type = 'image/x-icon';
+      document.getElementsByTagName('head')[0].appendChild(link);
+    }
+    link.href = 'cryptic-favicon.ico';
 
     this.apiService.loadData(cryptic ? 'cryptic' : 'morpheus');
   }
