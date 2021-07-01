1bee46f9eee9b7c92c558a56eb7ec4d04acff72a
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2021-07-01 15:23:37 +0000

    Added secshell

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit} from '@angular/core';
-import {ApiService} from "./core/api.service";
-import {Namespace, ServiceQuery} from "./core/data";
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from "./core/api.service";
+import { Namespace, ServiceQuery } from "./core/data";
 
 @Component({
   selector: 'app-root',
@@ -23,19 +23,35 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    const cryptic = window.location.hostname.includes('cryptic');
+    const brand = this.getBrand();
+    const morpheus = brand === 'the-morpheus';
 
-    if (cryptic) {
-      let link = document.querySelector<HTMLLinkElement>("link[rel~='icon']");
-      if (!link) {
-        link = document.createElement('link');
-        link.rel = 'icon';
-        link.type = 'image/x-icon';
-        document.getElementsByTagName('head')[0].appendChild(link);
-      }
-      link.href = 'cryptic-favicon.ico';
+    let link = document.querySelector<HTMLLinkElement>("link[rel~='icon']");
+    if (!link && !morpheus) {
+      link = document.createElement('link');
+      link.rel = 'icon';
+      link.type = 'image/x-icon';
+      document.getElementsByTagName('head')[0].appendChild(link);
     }
 
-    this.apiService.loadData(cryptic ? 'cryptic' : 'morpheus');
+    switch (brand) {
+      case 'cryptic-game':
+        link!.href = 'cryptic-favicon.ico';
+        this.apiService.loadData('cryptic');
+        break;
+      case 'secshell':
+        link!.href = 'secshell-favicon.ico';
+        this.apiService.loadData('sec_shell');
+        break;
+      default:
+        this.apiService.loadData('morpheus');
+    }
+  }
+
+  private getBrand(): string {
+    const hostname = window.location.hostname;
+
+    let tmp = hostname.substring(0, hostname.lastIndexOf('.'));
+    return tmp.substring(tmp.lastIndexOf('.') + 1);
   }
 }
